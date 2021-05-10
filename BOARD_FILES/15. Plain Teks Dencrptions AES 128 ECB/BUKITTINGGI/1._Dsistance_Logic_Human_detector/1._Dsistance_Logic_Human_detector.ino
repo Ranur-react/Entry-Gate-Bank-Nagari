817590ea98cf1c160a6b7abb5fa622693aab73c7
Ranur-react <rahmatnur845@gmail.com>
2021-05-11 06:09:18 +0700

    Selamat 1, Distance Adult detect OK

--- BOARD_FILES/15. Plain Teks Dencrptions AES 128 ECB/BUKITTINGGI/1._Dsistance_Logic_Human_detector/1._Dsistance_Logic_Human_detector.ino
+++ BOARD_FILES/15. Plain Teks Dencrptions AES 128 ECB/BUKITTINGGI/1._Dsistance_Logic_Human_detector/1._Dsistance_Logic_Human_detector.ino
@@ -1,3 +1,18 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <ArduinoJson.h>
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
+char server[]="192.168.100.100"; //Domain atau Address WEBSERVER
+String host="Host: 192.168.100.100"; //Domain atau Address WEBSERVER
+//IPAddress ip(192,168,137,2);//IP Address Ethernet Shield
+IPAddress  localBoardIP;
+EthernetClient client;
+//------------------------------------
+
+int GateCOM=20;
+int GateCOM2=21;
+String kode="";
+boolean state=false;
 // defines variables Sensor Ultra Sonic---------
 const int trigPin = 16;
 const int echoPin = 17;
@@ -22,7 +37,114 @@ distance= duration*0.034/2;
 //Serial.println(distance);
 //return distance;
 }
+void ToBoolean(String value)
+    {
+        if(value =="true" ){
+          
+          state=true;
+          }else{
+            state=false;  
+          }
+    }
+ void Cetak(String Display,String Value){
+  if(Display == "S"){
+  Serial.println(Value);
+    }else{
+      //print with Other Display
+      }
+  }
+  void GetFromDb(String value){
+   
+     value.trim();    
+     String valueGet="data="+value;
+     Cetak("S","Log Value");
+     Cetak("S",valueGet);
+     
+    if (client.connect(server, 80)) {    
+        Cetak("S","POST Connect");
+        client.println("POST /banknagari/Entry-Gate-Bank-Nagari/GATE_TIKET/index.php HTTP/1.1");
+        client.println(host);
+        client.println("Content-Type: application/x-www-form-urlencoded");
+        client.println("Connection: close");
+        client.print("Content-Length: ");
+        client.println(valueGet.length());
+        client.println();
+        client.print(valueGet);
+        client.println();
 
+        client.println();
+         delay(100);
+                String data= client.readString();
+//                Serial.println(data);
+                                int indexBatasAwal=data.indexOf("_start_");
+                Serial.println("Mulai Index Karakter String seharusnya: ");
+                Serial.println(indexBatasAwal);
+                Serial.println("Mulai Karakter String seharusnya: ");
+                String json=(String)data.substring(indexBatasAwal+8);
+                Serial.println(json);
+                ToBoolean(json);
+
+    }else{
+     Cetak("S","Connections Filed ..!!");
+     delay(100);
+    }
+    client.stop();
+    }
+
+ void countDataAdultDB(){
+   
+
+     
+    if (client.connect(server, 80)) {    
+        Cetak("S","POST Connect");
+        client.println("GET /banknagari/Entry-Gate-Bank-Nagari/GATE_TIKET/adult_Count.php HTTP/1.1");
+        client.println(host);
+        client.println("Content-Type: application/x-www-form-urlencoded");
+        client.println("Connection: close");
+        client.print("Content-Length: ");
+        client.println(valueGet.length());
+        client.println();
+        client.print(valueGet);
+        client.println();
+
+        client.println();
+         delay(100);
+                String data= client.readString();
+//                Serial.println(data);
+                                int indexBatasAwal=data.indexOf("_start_");
+                Serial.println("Mulai Index Karakter String seharusnya: ");
+                Serial.println(indexBatasAwal);
+                Serial.println("Mulai Karakter String seharusnya: ");
+                String json=(String)data.substring(indexBatasAwal+8);
+                Serial.println(json);
+                ToBoolean(json);
+
+    }else{
+     Cetak("S","Connections Filed ..!!");
+     delay(100);
+    }
+    client.stop();
+    }
+
+
+void inisialisasNet(){
+//    Ethernet.begin(mac);
+//  Serial.println("Conncted on Local Network with your IP:");
+//  localBoardIP=Ethernet.localIP();
+//  Serial.println(localBoardIP);
+//    Serial.println("Conncted on Local Network with your IP:");
+//  localBoardIP=Ethernet.localIP();
+   if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }}
+    Serial.print("Connected");
+}
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -40,6 +162,7 @@ if (distance<60){
   delay(200);
   if (distance<60){
       Serial.println("Adult Man Detected");
+      countDataAdultDB();
         delay(3000);
       while(true){
 
