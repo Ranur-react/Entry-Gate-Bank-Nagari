9660998afdebda303487869f28ac81c637b1c6a4
Ranur-react <rahmatnur845@gmail.com>
2021-05-11 13:48:34 +0700

    pload file Test QR

--- GATE_TIKET/Testqr.php
+++ GATE_TIKET/Testqr.php
@@ -6,6 +6,7 @@
 			$chiper=$_GET['data'];
 			// echo "Nulll";
 		}
+		echo "$chiper";
 	mysqli_query($conn,"INSERT INTO `tb_log` ( id,log,jenis_log, gate_id,date) VALUES (NULL, 'Coba ESP QR-CodeValue---->$chiper','error', '05',now());");
 
  ?>
