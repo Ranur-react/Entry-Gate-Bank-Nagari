cd78bb802f7ab8ef19fda9565151f2dc8eab6fb5
Ranur-react <rahmatnur845@gmail.com>
2021-05-20 03:32:15 +0700

    Perbaiki Dewasa main aman

--- GATE_TIKET/tes_Post.php
+++ GATE_TIKET/tes_Post.php
@@ -45,7 +45,26 @@ if($data['levelValue'] =='0001' & $adult=='true'){
 							 }
 						}
 }else if($data['levelValue'] =='0001' & $adult=='false'){
-	echo "Palsu Dewasa REJECT";
+		$qry = mysqli_fetch_array(mysqli_query($conn,"SELECT*FROM `tb_tiketmasuk` WHERE `id_denc_serial`='$serial';"));
+						if (!empty($qry['id_denc_serial'])) {
+							mysqli_query($conn,"INSERT INTO `tb_log` ( id,log,jenis_log, gate_id,date) VALUES (NULL, 'iD Tiket .$plainteks. yang digunakan sudah Tidak Berlaku karena sudah digunakan','already_used', '05',now());");
+						echo "false";	
+						}else{
+							$level=$data['levelValue'];
+							$nomorbuktiValue=$data['nomorbuktiValue'];
+							 $date_serial=dateFormat($data['dateValue']);
+							 $time_serial=timeFormat($data['dateValue']);
+							 if (dateEqualas($date_serial)) {
+							 	SensorCountHumans($conn,$plainteks);
+								mysqli_query($conn,"INSERT INTO `db_banknagari`.`tb_tiketmasuk` (`id_ecn`, `id_denc_serial`, `date_serial`, `time_serial`, `level_tiket`, `nomor_bukti`, `date_in`, `id_gate`) VALUES ('$chipperteks', '$plainteks', '$date_serial', '$time_serial', '$level', '$nomorbuktiValue', NOW(), '05')");
+							 	# code...
+								echo "true";	
+							 }else{
+							mysqli_query($conn,"INSERT INTO `tb_log` ( id,log,jenis_log, gate_id,date) VALUES (NULL, 'iD Tiket .$plainteks. yang digunakan sudah Tidak Berlaku /Kdaluarsa','ove_due', '05',now());");
+
+								echo "Kadaluarsa ";	
+							 }
+						}
 }
 //anak-anak state
 else if($data['levelValue'] =='0002' & $adult=='true'){
